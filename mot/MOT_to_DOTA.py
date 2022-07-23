21da3f2e373c9c97abebd521a5e299d507dd4467
Susancutie <2019300003067@whu.edu.cn>
2022-07-23 11:37:15 +0800

    fix(修复遍历顺序):

--- mot/MOT_to_DOTA.py
+++ mot/MOT_to_DOTA.py
@@ -1,18 +1,30 @@
 import os
 import shutil
 
-path = 'G:/大创项目/test_process_data/train'
-new_path = 'G:/大创项目/test_process_data/new_train'
+path = 'G:/大创项目/test_process_data/train/'
+new_path = 'G:/大创项目/test_process_data/new_train/'
 count = os.listdir(path)
 count.sort(key = int)
 print(count)
 num = 0
 
-for root, dirs, files in os.walk(path):
-    if len(dirs) == 0:
-        for file in files:
-            if file.find('.jpg') != -1:
-                shutil.copy(os.path.join(path +'/'+ root[-6] + root[-5] +'/'+ 'img', file),os.path.join(new_path, 'P'+ str(num).zfill(4) + '.jpg'))
-                num = num + 1
+
+
+for dir in count:
+    files = os.listdir(path+dir+'/img/')
+    #print(files)
+    for file in files:
+        if file.find('.jpg') != -1:
+            shutil.copy(os.path.join(path +'/'+ dir +'/'+ 'img', file),os.path.join(new_path, 'P'+ str(num).zfill(6) + '.jpg'))
+            num = num + 1
+
+
+
+# for root, dirs, files in os.walk(path):
+#     if len(dirs) == 0:
+#         for file in files:
+#             if file.find('.jpg') != -1:
+#                 shutil.copy(os.path.join(path +'/'+ root[-6] + root[-5] +'/'+ 'img', file),os.path.join(new_path, 'P'+ str(num).zfill(4) + '.jpg'))
+#                 num = num + 1
 
 
